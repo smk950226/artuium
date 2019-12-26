3878075ff51dc5b93c887d64516a4a3c543c5a27
yang <yaaaaang57@naver.com>
2019-12-26 17:09:41 +0900

    gesturehandler err fix

--- artuium/config/urls.js
+++ artuium/config/urls.js
@@ -1,6 +1,6 @@
 // const FETCH_URL = 'http://127.0.0.1:8000'
 // const FETCH_URL = 'http://artuium.ap-northeast-2.elasticbeanstalk.com'
-const FETCH_URL = 'http://40487e8e.ngrok.io'
+const FETCH_URL = 'http://3f4f42cc.ngrok.io'
 const PUBLIC_URL = ''
 
 export {
