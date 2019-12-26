c4ab1d8173c2d4b6d21861b60d03943f2031997c
yang <yaaaaang57@naver.com>
2019-12-27 05:23:48 +0900

    d

--- artuium/config/urls.js
+++ artuium/config/urls.js
@@ -1,6 +1,6 @@
 // const FETCH_URL = 'http://127.0.0.1:8000'
-// const FETCH_URL = 'http://artuium.ap-northeast-2.elasticbeanstalk.com'
-const FETCH_URL = 'http://6f2c754c.ngrok.io'
+const FETCH_URL = 'http://artuium.ap-northeast-2.elasticbeanstalk.com'
+// const FETCH_URL = 'http://6f2c754c.ngrok.io'
 const PUBLIC_URL = ''
 
 export {
