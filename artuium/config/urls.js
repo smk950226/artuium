e9220b21520314c786cf9f832af67bd837289022
smk950226 <smk950226@naver.com>
2019-12-26 11:16:31 +0900

    change to me

--- artuium/config/urls.js
+++ artuium/config/urls.js
@@ -1,6 +1,6 @@
 // const FETCH_URL = 'http://127.0.0.1:8000'
 // const FETCH_URL = 'http://artuium.ap-northeast-2.elasticbeanstalk.com'
-const FETCH_URL = 'http://40487e8e.ngrok.io'
+const FETCH_URL = 'http://f0c3aaab.ngrok.io'
 const PUBLIC_URL = ''
 
 export {
