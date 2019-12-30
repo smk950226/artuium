2c4520c65f457a49feacf2f17a0350779a8a98e3
smk950226 <smk950226@naver.com>
2019-12-30 14:49:31 +0900

    change bundle id

--- artuium/android/app/src/main/java/com/artuium/fov/MainActivity.java
+++ artuium/android/app/src/main/java/com/artuium/fov/MainActivity.java
@@ -1,4 +1,4 @@
-package com.artuium;
+package com.artuium.fov;
 
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
--- artuium/android/app/src/main/java/com/artuium/fov/SplashActivity.java
+++ artuium/android/app/src/main/java/com/artuium/fov/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.artuium;
+package com.artuium.fov;
 
 import android.content.Intent;
 import android.os.Bundle;
