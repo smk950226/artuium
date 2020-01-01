6cf51d32f301b290b30244a688dc13ba0b742b5f
smk950226 <smk950226@naver.com>
2020-01-02 01:10:08 +0900

    routing review detail

--- artuium/config/urls.js
+++ artuium/config/urls.js
@@ -1,6 +1,6 @@
 // const FETCH_URL = 'http://127.0.0.1:8000'
 // const FETCH_URL = 'http://artuium.ap-northeast-2.elasticbeanstalk.com'
-const FETCH_URL = 'http://432d0663.ngrok.io'
+const FETCH_URL = 'https://artuium.shop'
 const PUBLIC_URL = 'http://artuium.ap-northeast-2.elasticbeanstalk.com'
 
 export {
