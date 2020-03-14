125d3d544f0c4cf5c4e255d0b06e232dc5923e32
KimSeungHu <mind9063@kaist.ac.kr>
2020-03-14 14:48:40 +0900

    implement artium header

--- artuium/assets/images/index.js
+++ artuium/assets/images/index.js
@@ -3,8 +3,10 @@ export const chatNumIconGrey = require('./chatNumIconGrey.png');
 export const heartNumIcon = require('./heartNumIcon.png');
 export const heartNumIconGrey = require('./heartNumIconGrey.png');
 
-export const emptyFollowerIllust = require('./emptyFollowerIllust.png');
-
 export const fullStar = require('./fullStar.png');
 export const halfStar = require('./halfStar.png');
 export const emptyStar = require('./emptyStar.png');
+
+export const emptyFollowerIllust = require('./emptyFollowerIllust.png');
+
+export const backArrow = require('./backArrow.png');
