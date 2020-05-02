091dd3bcda55dc1d6f24171b500e6ce13f6ed922
오수경 <osugyeong@osugyeong-ui-MacBookPro.local>
2020-05-02 16:47:38 +0900

    [dev-change-notification-ui] Change notification ui

--- artuium/components/Notice/presenter.js
+++ artuium/components/Notice/presenter.js
@@ -1,62 +1,136 @@
 import React from 'react';
-import { View, Text, TouchableWithoutFeedback, Image, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Image,
+  Dimensions,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../../styles';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
-const Notice = (props) => (
-    <View style={[styles.borderBtmGrayE6, styles.bgGrayF8]}>
-        <View style={[styles.pt25, styles.pb20, styles.px20]}>
-            <View style={[styles.row, styles.justifyContentBetween]}>
-                <View style={[styles.flex9]}>
-                    <View style={[styles.row, styles.alignItemsCenter, styles.justifyContentBetween]}>
-                        <View style={[styles.row, styles.alignItemsCenter, styles.justifyContentBetween]}>
-                            <View style={[styles.bgRed, styles.circle6, styles.mr5, props.is_new ? null : styles.hidden]} />
-                            <Text style={[styles.fontBold, styles.font13]}>
-                                {props.notice.title}
-                            </Text>
-                        </View>
-                        <TouchableWithoutFeedback onPress={props.expand ? props.closeExpand : props.openExpand}>
-                            <View >
-                                <Text style={[styles.fontBlack, styles.font16]}>
-                                    {props.expand ? (
-                                        <Image source={require('../../assets/images/arrow_up.png')} style={[{width: 40, height: 40}]} />
-                                    ) : (
-                                        <Image source={require('../../assets/images/arrow_down.png')} style={[{width: 40, height: 40}]} />
-                                    )}
-                                </Text>
-                            </View>
-                        </TouchableWithoutFeedback>
-                    </View>
-                    <Text style={[styles.fontMedium, styles.font13, styles.grayD1, styles.ml10]}>
-                        {`${props.notice.date.slice(0,4)}.${props.notice.date.slice(5,7)}.${props.notice.date.slice(8,10)}`}
-                    </Text>
-                </View>
+const Notice = props => (
+  <View style={[styles.borderBtmGrayE6]}>
+    <View style={[styles.pt20, styles.pb25, {paddingHorizontal: 26}]}>
+      <View style={[styles.row, styles.justifyContentBetween]}>
+        <View style={[styles.flex9]}>
+          <View
+            style={[
+              styles.row,
+              styles.alignItemsCenter,
+              styles.justifyContentBetween,
+            ]}>
+            <View
+              style={[
+                styles.row,
+                styles.alignItemsCenter,
+                styles.justifyContentBetween,
+              ]}>
+              <View
+                style={[
+                  props.is_new ? null : styles.hidden,
+                  {
+                    backgroundColor: '#FA4D2C',
+                    position: 'absolute',
+                    left: -8,
+                    width: 4,
+                    height: 4,
+                    borderRadius: 2,
+                  },
+                ]}
+              />
+              <Text
+                style={{
+                  fontSize: 13,
+                  lineHeight: 22,
+                  letterSpacing: -0.24,
+                  color: '#2E2E2E',
+                  opacity: 0.8,
+                }}>
+                {props.notice.title}
+              </Text>
             </View>
-            {props.expand && (
-                <View style={[styles.mt15]}>
-                    {props.notice.image ? (
-                        <Image resizeMode={'cover'} source={{uri: props.notice.image}} resizeMode={'contain'} style={[styles.alignSelfCenter, {height: props.notice.image_width >= props.notice.image_height ? props.notice.image_height*(width - 40)/props.notice.image_width : 400, width: props.notice.image_width >= props.notice.image_height ? width - 40 : props.notice.image_width*400/props.notice.image_height}]} />
-                    ) : (
-                        null
-                    )}
-                    <Text style={[styles.fontMedium, styles.font13, styles.mt10, styles.gray71, { lineHeight: 20 }]}>
-                        {props.notice.content}
-                    </Text>
-                </View>
-            )}
-            
+            <TouchableWithoutFeedback
+              onPress={props.expand ? props.closeExpand : props.openExpand}>
+              {props.expand ? (
+                <Image
+                  source={require('../../assets/images/upArrowIcon.png')}
+                  style={{width: 24, height: 24}}
+                />
+              ) : (
+                <Image
+                  source={require('../../assets/images/downArrowIcon.png')}
+                  style={{width: 24, height: 24}}
+                />
+              )}
+            </TouchableWithoutFeedback>
+          </View>
+          <Text
+            style={{
+              fontSize: 14,
+              lineHeight: 25,
+              letterSpacing: -0.24,
+              color: '#2E2E2E',
+              opacity: 0.3,
+            }}>
+            {`${props.notice.date.slice(0, 4)}.${props.notice.date.slice(
+              5,
+              7,
+            )}.${props.notice.date.slice(8, 10)}`}
+          </Text>
         </View>
+      </View>
+      {props.expand && (
+        <View style={[styles.mt15]}>
+          {props.notice.image ? (
+            <Image
+              resizeMode={'cover'}
+              source={{uri: props.notice.image}}
+              resizeMode={'contain'}
+              style={[
+                styles.alignSelfCenter,
+                {
+                  height:
+                    props.notice.image_width >= props.notice.image_height
+                      ? (props.notice.image_height * (width - 52)) /
+                        props.notice.image_width
+                      : 400,
+                  width:
+                    props.notice.image_width >= props.notice.image_height
+                      ? width - 52
+                      : (props.notice.image_width * 400) /
+                        props.notice.image_height,
+                },
+              ]}
+            />
+          ) : null}
+          <Text
+            style={[
+              styles.mt10,
+              {
+                fontSize: 12,
+                lineHeight: 20,
+                letterSpacing: -0.24,
+                color: '#2E2E2E',
+                opacity: 0.6,
+              },
+            ]}>
+            {props.notice.content}
+          </Text>
+        </View>
+      )}
     </View>
-)
+  </View>
+);
 
 Notice.propTypes = {
-    openExpand: PropTypes.func.isRequired,
-    closeExpand: PropTypes.func.isRequired,
-    expand: PropTypes.bool.isRequired,
-    notice: PropTypes.object.isRequired,
-    is_new: PropTypes.bool.isRequired
-}
+  openExpand: PropTypes.func.isRequired,
+  closeExpand: PropTypes.func.isRequired,
+  expand: PropTypes.bool.isRequired,
+  notice: PropTypes.object.isRequired,
+  is_new: PropTypes.bool.isRequired,
+};
 
 export default Notice;
--- artuium/components/Notification/presenter.js
+++ artuium/components/Notification/presenter.js
@@ -1,143 +1,250 @@
-import React, { Fragment } from 'react';
-import { View, Text, TouchableWithoutFeedback, Image, Dimensions } from 'react-native';
+import React, {Fragment} from 'react';
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Image,
+  Dimensions,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../../styles';
 
-const { width, height } = Dimensions.get('window')
+const {width, height} = Dimensions.get('window');
 
-const Notification = (props) => (
-    <TouchableWithoutFeedback onPress={props.openExpand}>
-    <View style={[styles.borderBtmGrayE6, styles.bgGrayF8]}>
-        <View style={[styles.pt25, styles.pb20, styles.px20]}>
-            <View style={[styles.row, styles.alignItemsCenter, styles.justifyContentBetween]}>
-                <View style={[styles.flex9]}>
-                    <View style={[styles.row]}>
-                        <View style={[styles.bgRed, styles.circle6, styles.mr5, styles.mt10, props.is_new ? null : styles.hidden]} />
-                        <View style={[styles.row, styles.flexWrap]}>
-                            {props.notification.type === 'comment_review' && (
-                                <Fragment>
-                                    <Text style={[styles.fontBold, styles.font13]}>{props.notification.from_user.nickname}</Text>
-                                    <Text style={[styles.fontMedium, styles.font13]}>
-                                    님이 회원님의 
-                                    </Text>
-                                    {props.notification.review && (
-                                        <Text style={[styles.fontBold, styles.font13]}>
-                                            {props.notification.review.exhibition && ` '${props.notification.review.exhibition.name}'`}
-                                            {props.notification.review.artwork && ` '${props.notification.review.artwork.name}'`}
-                                        </Text>
-                                    )}
-                                    {props.notification.review && (
-                                        <Text style={[styles.fontMedium, styles.font13]}>
-                                            {props.notification.review.exhibition && ` 전시 감상에 `}
-                                            {props.notification.review.artwork && ` 감상에 `}
-                                        </Text>
-                                    )}
-                                    <Text style={[styles.fontMedium, styles.font13]}>
-                                    댓글을 달았습니다.
-                                    </Text>
-                                </Fragment>
-                            )}
-                            {props.notification.type === 'comment_reply' && (
-                                <Fragment>
-                                    <Text style={[styles.fontBold, styles.font13]}>{props.notification.from_user.nickname}</Text>
-                                    <Text style={[styles.fontMedium, styles.font13]}>
-                                    님이 회원님의 
-                                    </Text>
-                                    {props.notification.reply && (
-                                        <Text style={[styles.fontBold, styles.font13]}>
-                                            {props.notification.review.exhibition && ` '${props.notification.review.exhibition.name}'`}
-                                            {props.notification.review.artwork && ` '${props.notification.review.artwork.name}'`}
-                                        </Text>
-                                    )}
-                                    {props.notification.reply && (
-                                        <Text style={[styles.fontMedium, styles.font13]}>
-                                            {props.notification.review.exhibition && ` 전시 감상의 댓글에 `}
-                                            {props.notification.review.artwork && ` 감상의 댓글에 `}
-                                        </Text>
-                                    )}
-                                    <Text style={[styles.fontMedium, styles.font13]}>
-                                    대댓글을 달았습니다.
-                                    </Text>
-                                </Fragment>
-                            )}
-                            {props.notification.type === 'like_review' && (
-                                <Fragment>
-                                    <Text style={[styles.fontBold, styles.font13]}>{props.notification.from_user.nickname}</Text>
-                                    <Text style={[styles.fontMedium, styles.font13]}>
-                                    님이 회원님의 
-                                    </Text>
-                                    {props.notification.review && (
-                                        <Text style={[styles.fontBold, styles.font13]}>
-                                            {props.notification.review.exhibition && ` '${props.notification.review.exhibition.name}'`}
-                                            {props.notification.review.artwork && ` '${props.notification.review.artwork.name}'`}
-                                        </Text>
-                                    )}
-                                    {props.notification.review && (
-                                        <Text style={[styles.fontMedium, styles.font13]}>
-                                        {props.notification.review.exhibition && ` 전시 감상에 `}
-                                        {props.notification.review.artwork && ` 감상에 `}
-                                        </Text>
-                                    )}
-                                    <Text style={[styles.fontMedium, styles.font13]}>
-                                    좋아요를 눌렀습니다.
-                                    </Text>
-                                </Fragment>
-                            )}
-                            {props.notification.type === 'following' && (
-                                <Fragment>
-                                    <Text style={[styles.fontBold, styles.font13]}>{props.notification.from_user.nickname}</Text>
-                                    <Text style={[styles.fontMedium, styles.font13]}>
-                                    님이 회원님을 팔로우합니다.
-                                    </Text>
-                                </Fragment>
-                            )}
-                        </View>
-                    </View>
-                    <Text style={[styles.fontMedium, styles.font13, styles.grayD1, {marginLeft: 11}]}>
-                        {`${props.notification.date.slice(0,4)}.${props.notification.date.slice(5,7)}.${props.notification.date.slice(8,10)}`}
-                    </Text>
-                </View>
+const Notification = props => (
+  <TouchableWithoutFeedback onPress={props.openExpand}>
+    <View style={[styles.borderBtmGrayE6]}>
+      <View style={[styles.pt25, styles.pb20, {paddingHorizontal: 26}]}>
+        <View
+          style={[
+            styles.row,
+            styles.alignItemsCenter,
+            styles.justifyContentBetween,
+          ]}>
+          <View style={[styles.flex9]}>
+            <View style={[styles.row]}>
+              <View
+                style={[
+                  styles.mt10,
+                  props.is_new ? null : styles.hidden,
+                  {
+                    backgroundColor: '#FA4D2C',
+                    position: 'absolute',
+                    left: -8,
+                    width: 4,
+                    height: 4,
+                    borderRadius: 2,
+                  },
+                ]}
+              />
+              <View style={[styles.row, styles.flexWrap]}>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    lineHeight: 22,
+                    letterSpacing: -0.24,
+                    color: '#2E2E2E',
+                    opacity: 0.8,
+                  }}>
+                  {props.notification.type === 'comment_review' && (
+                    <Fragment>
+                      <Text style={{fontWeight: 'bold'}}>
+                        {props.notification.from_user.nickname}
+                      </Text>
+                      님이 회원님의
+                      {props.notification.review && (
+                        <Text style={{fontWeight: 'bold'}}>
+                          {props.notification.review.exhibition &&
+                            ` '${props.notification.review.exhibition.name}'`}
+                          {props.notification.review.artwork &&
+                            ` '${props.notification.review.artwork.name}'`}
+                        </Text>
+                      )}
+                      {props.notification.review && (
+                        <Text>
+                          {props.notification.review.exhibition &&
+                            ` 전시 감상에 `}
+                          {props.notification.review.artwork && ` 감상에 `}
+                        </Text>
+                      )}
+                      댓글을 달았습니다.
+                    </Fragment>
+                  )}
+                  {props.notification.type === 'comment_reply' && (
+                    <Fragment>
+                      <Text style={{fontWeight: 'bold'}}>
+                        {props.notification.from_user.nickname}
+                      </Text>
+                      님이 회원님의
+                      {props.notification.reply && (
+                        <Text style={{fontWeight: 'bold'}}>
+                          {props.notification.review.exhibition &&
+                            ` '${props.notification.review.exhibition.name}'`}
+                          {props.notification.review.artwork &&
+                            ` '${props.notification.review.artwork.name}'`}
+                        </Text>
+                      )}
+                      {props.notification.reply && (
+                        <Text>
+                          {props.notification.review.exhibition &&
+                            ` 전시 감상의 댓글에 `}
+                          {props.notification.review.artwork &&
+                            ` 감상의 댓글에 `}
+                        </Text>
+                      )}
+                      대댓글을 달았습니다.
+                    </Fragment>
+                  )}
+                  {props.notification.type === 'like_review' && (
+                    <Fragment>
+                      <Text style={{fontWeight: 'bold'}}>
+                        {props.notification.from_user.nickname}
+                      </Text>
+                      님이 회원님의
+                      {props.notification.review && (
+                        <Text style={{fontWeight: 'bold'}}>
+                          {props.notification.review.exhibition &&
+                            ` '${props.notification.review.exhibition.name}'`}
+                          {props.notification.review.artwork &&
+                            ` '${props.notification.review.artwork.name}'`}
+                        </Text>
+                      )}
+                      {props.notification.review && (
+                        <Text>
+                          {props.notification.review.exhibition &&
+                            ` 전시 감상에 `}
+                          {props.notification.review.artwork && ` 감상에 `}
+                        </Text>
+                      )}
+                      좋아요를 눌렀습니다.
+                    </Fragment>
+                  )}
+                  {props.notification.type === 'following' && (
+                    <Fragment>
+                      <Text style={{fontWeight: 'bold'}}>
+                        {props.notification.from_user.nickname}
+                      </Text>
+                      님이 회원님을 팔로우합니다.
+                    </Fragment>
+                  )}
+                </Text>
+              </View>
             </View>
-            {props.notification.type === 'comment_review' && (
-                <View style={[styles.mt10, styles.px10, styles.py10, styles.bgWhite, styles.borderGrayE6, styles.borderRadius5]}>
-                    <View style={[styles.row]}>
-                        <View style={[styles.flex1]}>
-                            <View style={[styles.row, styles.alignItemsCenter]}>
-                                {props.notification.reply.author.profile_image ? (
-                                    <Image source={{uri: props.notification.reply.author.profile_image}} style={[styles.profileImage30]} resizeMode={'cover'} />
-                                ) : (
-                                    <Image source={require('../../assets/images/empty_profile.png')} style={[styles.profileImage30]} />
-                                )}
-                                <Text style={[styles.fontBold, styles.font14, styles.ml5]}>{props.notification.reply.author.nickname}</Text>
-                                <Text style={[styles.fontBold, styles.font11, styles.grayBa, styles.ml5]}>{`${props.notification.reply.time.slice(0,4)}.${props.notification.reply.time.slice(5,7)}.${props.notification.reply.time.slice(8,10)}`}</Text>
-                            </View>
-                            <Text style={[styles.fontRegular, styles.font11, styles.gray71, styles.mt10, styles.mr5]} numberOfLines={4}>
-                                {props.notification.reply.content}
-                            </Text>
-                        </View>
-                        <View style={[{width: 100}, styles.justifyContentCenter]}>
-                            {props.notification.review.exhibition && (
-                                <Image source={{uri: props.notification.review.exhibition.images && props.notification.review.exhibition.images.length > 0 ? props.notification.review.exhibition.images[0].image : ''}} style={[styles.borderRadius10, {width: 100, height: 100}]} resizeMode={'cover'} />
-                            )}
-                            {props.notification.review.artwork && (
-                                <Image source={{uri: props.notification.review.artwork.image ? props.notification.review.artwork.image : ''}} style={[styles.widthFull]} style={[styles.borderRadius10, {width: 100, height: 100}]} resizeMode={'cover'} />
-                            )}
-                        </View>
-                        
-                    </View>
-                </View>
-            )}
+            <Text
+              style={{
+                fontSize: 14,
+                lineHeight: 25,
+                letterSpacing: -0.24,
+                color: '#2E2E2E',
+                opacity: 0.3,
+              }}>
+              {`${props.notification.date.slice(
+                0,
+                4,
+              )}.${props.notification.date.slice(
+                5,
+                7,
+              )}.${props.notification.date.slice(8, 10)}`}
+            </Text>
+          </View>
         </View>
+        {props.notification.type === 'comment_review' && (
+          <View
+            style={[
+              styles.mt10,
+              styles.px10,
+              styles.py10,
+              styles.bgWhite,
+              styles.borderGrayE6,
+              styles.borderRadius5,
+            ]}>
+            <View style={[styles.row]}>
+              <View style={[styles.flex1]}>
+                <View style={[styles.row, styles.alignItemsCenter]}>
+                  {props.notification.reply.author.profile_image ? (
+                    <Image
+                      source={{
+                        uri: props.notification.reply.author.profile_image,
+                      }}
+                      style={[styles.profileImage30]}
+                      resizeMode={'cover'}
+                    />
+                  ) : (
+                    <Image
+                      source={require('../../assets/images/empty_profile.png')}
+                      style={[styles.profileImage30]}
+                    />
+                  )}
+                  <Text style={[styles.fontBold, styles.font14, styles.ml5]}>
+                    {props.notification.reply.author.nickname}
+                  </Text>
+                  <Text
+                    style={[
+                      styles.fontBold,
+                      styles.font11,
+                      styles.grayBa,
+                      styles.ml5,
+                    ]}>{`${props.notification.reply.time.slice(
+                    0,
+                    4,
+                  )}.${props.notification.reply.time.slice(
+                    5,
+                    7,
+                  )}.${props.notification.reply.time.slice(8, 10)}`}</Text>
+                </View>
+                <Text
+                  style={[
+                    styles.fontRegular,
+                    styles.font11,
+                    styles.gray71,
+                    styles.mt10,
+                    styles.mr5,
+                  ]}
+                  numberOfLines={4}>
+                  {props.notification.reply.content}
+                </Text>
+              </View>
+              <View style={[{width: 100}, styles.justifyContentCenter]}>
+                {props.notification.review.exhibition && (
+                  <Image
+                    source={{
+                      uri:
+                        props.notification.review.exhibition.images &&
+                        props.notification.review.exhibition.images.length > 0
+                          ? props.notification.review.exhibition.images[0].image
+                          : '',
+                    }}
+                    style={[styles.borderRadius10, {width: 100, height: 100}]}
+                    resizeMode={'cover'}
+                  />
+                )}
+                {props.notification.review.artwork && (
+                  <Image
+                    source={{
+                      uri: props.notification.review.artwork.image
+                        ? props.notification.review.artwork.image
+                        : '',
+                    }}
+                    style={[styles.widthFull]}
+                    style={[styles.borderRadius10, {width: 100, height: 100}]}
+                    resizeMode={'cover'}
+                  />
+                )}
+              </View>
+            </View>
+          </View>
+        )}
+      </View>
     </View>
-    </TouchableWithoutFeedback>
-)
+  </TouchableWithoutFeedback>
+);
 
 Notification.propTypes = {
-    openExpand: PropTypes.func.isRequired,
-    closeExpand: PropTypes.func.isRequired,
-    expand: PropTypes.bool.isRequired,
-    notification: PropTypes.object.isRequired,
-    is_new: PropTypes.bool.isRequired
-}
+  openExpand: PropTypes.func.isRequired,
+  closeExpand: PropTypes.func.isRequired,
+  expand: PropTypes.bool.isRequired,
+  notification: PropTypes.object.isRequired,
+  is_new: PropTypes.bool.isRequired,
+};
 
 export default Notification;
