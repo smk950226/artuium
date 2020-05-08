2a9936209ac4d58e2e6dc3263934114da73b9266
오수경 <osugyeong@osugyeong-ui-MacBookPro.local>
2020-05-08 19:31:15 +0900

    [modify-search] Lint change

--- artuium/screens/SearchScreen/container.js
+++ artuium/screens/SearchScreen/container.js
@@ -1,64 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import SearchScreen from './presenter';
 
-class Container extends Component{
-    static propTypes = {
-        search: PropTypes.func.isRequired
-    }
+class Container extends Component {
+  static propTypes = {
+    search: PropTypes.func.isRequired,
+  };
 
-    state = {
-        q: "",
-        focused: false,
-        artworks: [],
-        exhibitions: []
-    }
+  state = {
+    q: '',
+    focused: false,
+    artworks: [],
+    exhibitions: [],
+  };
 
-    _handleQChange = async(q) => {
-        this.setState({
-            q
-        })
-        const result = await this.props.search(q)
-        if(result){
-            this.setState({
-                artworks: result.artworks,
-                exhibitions: result.exhibitions
-            })
-        }
+  _handleQChange = async q => {
+    this.setState({
+      q,
+    });
+    const result = await this.props.search(q);
+    if (result) {
+      this.setState({
+        artworks: result.artworks,
+        exhibitions: result.exhibitions,
+      });
     }
+  };
 
-    _onFocus = () => {
-        this.setState({
-            focused: true
-        })
-    }
+  _onFocus = () => {
+    this.setState({
+      focused: true,
+    });
+  };
 
-    _onBlur = () => {
-        this.setState({
-            focused: false
-        })
-    }
+  _onBlur = () => {
+    this.setState({
+      focused: false,
+    });
+  };
 
-    _makeBlank = () => {
-        this.setState({
-            q: "",
-            artworks: [],
-            exhibitions: []
-        })
-    }
+  _makeBlank = () => {
+    this.setState({
+      q: '',
+      artworks: [],
+      exhibitions: [],
+    });
+  };
 
-    render(){
-        return(
-            <SearchScreen 
-            {...this.props}
-            {...this.state}
-            handleQChange={this._handleQChange}
-            onFocus={this._onFocus}
-            onBlur={this._onBlur}
-            makeBlank={this._makeBlank}
-            />
-        )
-    }
+  render() {
+    return (
+      <SearchScreen
+        {...this.props}
+        {...this.state}
+        handleQChange={this._handleQChange}
+        onFocus={this._onFocus}
+        onBlur={this._onBlur}
+        makeBlank={this._makeBlank}
+      />
+    );
+  }
 }
 
 export default Container;
--- artuium/screens/SearchScreen/presenter.js
+++ artuium/screens/SearchScreen/presenter.js
@@ -1,141 +1,269 @@
 import React from 'react';
-import { View, Text, ScrollView, Image, TextInput, Dimensions, TouchableWithoutFeedback, ImageBackground, Modal } from 'react-native';
-import { getStatusBarHeight } from "react-native-status-bar-height";
-import ArtworkCard from '../../components/ArtworkCard'
-import ExhibitionCard2 from '../../components/ExhibitionCard2'
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TextInput,
+  Dimensions,
+  TouchableWithoutFeedback,
+  ImageBackground,
+  Modal,
+} from 'react-native';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+import ArtworkCard from '../../components/ArtworkCard';
+import ExhibitionCard2 from '../../components/ExhibitionCard2';
 import PropTypes from 'prop-types';
 import styles from '../../styles';
 
-const statusBarHeight = getStatusBarHeight()
+const statusBarHeight = getStatusBarHeight();
 
-const { width, height } = Dimensions.get('window')
+const {width, height} = Dimensions.get('window');
 
 const dummyList = [
-    {
-        id: -1
-    }
-]
+  {
+    id: -1,
+  },
+];
 
-
-const SearchScreen = (props) => (
-    <View style={[styles.container]}>
-        <View style={[{height:50, marginTop: statusBarHeight}, styles.bgWhite, styles.row, styles.alignItemsCenter, styles.justifyContentBetween, styles.px25, styles.borderBtmGrayDb]}>
-            <TouchableWithoutFeedback onPress={() => props.navigation.goBack(null)}>
-                <View style={[styles.pr20]}>
-                    <Image source={require('../../assets/images/icon_back.png')} style={[{width: 9*1.6, height: 17*1.6}]} />
-                </View>
+const SearchScreen = props => (
+  <View style={[styles.container]}>
+    <View
+      style={[
+        {height: 50, marginTop: statusBarHeight},
+        styles.bgWhite,
+        styles.row,
+        styles.alignItemsCenter,
+        styles.justifyContentBetween,
+        styles.px25,
+        styles.borderBtmGrayDb,
+      ]}>
+      <TouchableWithoutFeedback onPress={() => props.navigation.goBack(null)}>
+        <View style={[styles.pr20]}>
+          <Image
+            source={require('../../assets/images/icon_back.png')}
+            style={[{width: 9 * 1.6, height: 17 * 1.6}]}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Text style={[styles.fontBold, styles.font18]}>통합 검색</Text>
+      <TouchableWithoutFeedback>
+        <View style={[styles.hidden]}>
+          <Image
+            source={require('../../assets/images/search.png')}
+            style={[{width: 17 * 1.6, height: 17 * 1.6}]}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+    <View
+      style={[
+        styles.row,
+        styles.alignItemsCenter,
+        styles.justifyContentBetween,
+        styles.px15,
+        styles.py10,
+        styles.bgGrayF4,
+      ]}>
+      <TouchableWithoutFeedback onPress={props.onFocus}>
+        <View
+          style={[
+            styles.bgWhite,
+            styles.borderRadius5,
+            styles.pl20,
+            styles.width90,
+            styles.justifyContentCenter,
+            {zIndex: 9, height: 43},
+          ]}>
+          <Text style={[styles.fontMedium, styles.font17]}>{props.q}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={props.makeBlank}>
+        <View style={[{zIndex: 9}]}>
+          <Image
+            source={require('../../assets/images/icon_close.png')}
+            style={[styles.icon15]}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+    {props.focused && (
+      <TouchableWithoutFeedback onPress={props.onBlur}>
+        <View
+          style={[
+            {
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              zIndex: 1,
+            },
+            styles.bgBlack07,
+          ]}>
+          <View
+            style={[
+              {height: 50, marginTop: statusBarHeight},
+              styles.bgWhite,
+              styles.row,
+              styles.alignItemsCenter,
+              styles.justifyContentBetween,
+              styles.px25,
+              styles.borderBtmGrayDb,
+              styles.hidden,
+            ]}>
+            <TouchableWithoutFeedback>
+              <View style={[styles.pr20]}>
+                <Image
+                  source={require('../../assets/images/icon_back.png')}
+                  style={[{width: 9 * 1.6, height: 17 * 1.6}]}
+                />
+              </View>
             </TouchableWithoutFeedback>
             <Text style={[styles.fontBold, styles.font18]}>통합 검색</Text>
             <TouchableWithoutFeedback>
-                <View style={[styles.hidden]}>
-                    <Image source={require('../../assets/images/search.png')} style={[{width: 17*1.6, height: 17*1.6}]} />
-                </View>
+              <View style={[styles.hidden]}>
+                <Image
+                  source={require('../../assets/images/search.png')}
+                  style={[{width: 17 * 1.6, height: 17 * 1.6}]}
+                />
+              </View>
             </TouchableWithoutFeedback>
-        </View>
-        <View style={[styles.row, styles.alignItemsCenter, styles.justifyContentBetween, styles.px15, styles.py10, styles.bgGrayF4]}>
-            <TouchableWithoutFeedback onPress={props.onFocus}>
-                    <View style={[styles.bgWhite, styles.borderRadius5, styles.pl20, styles.width90, styles.justifyContentCenter, {zIndex: 9, height: 43}]} >
-                        <Text style={[styles.fontMedium, styles.font17]}>{props.q}</Text>
-                    </View>
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback onPress={props.makeBlank}>
-                <View style={[{zIndex: 9}]}>
-                    <Image source={require('../../assets/images/icon_close.png')} style={[styles.icon15]} />
-                </View>
-            </TouchableWithoutFeedback>
-        </View>
-        {props.focused && (
+          </View>
+          <View
+            style={[
+              styles.row,
+              styles.alignItemsCenter,
+              styles.justifyContentBetween,
+              styles.px15,
+              styles.py10,
+              styles.bgTransparent,
+              {zIndex: 9},
+            ]}>
+            <TextInput
+              style={[
+                styles.bgWhite,
+                styles.black,
+                styles.borderRadius5,
+                styles.font14,
+                styles.pl20,
+                styles.width90,
+                styles.justifyContentCenter,
+                {zIndex: 9, height: 43},
+              ]}
+              underlineColorAndroid={'transparent'}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+              value={props.q}
+              onChangeText={props.handleQChange}
+              returnKeyType={'send'}
+              onFocus={props.onFocus}
+              onBlur={props.onBlur}
+              autoFocus={true}
+            />
             <TouchableWithoutFeedback onPress={props.onBlur}>
-            <View style={[{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 1}, styles.bgBlack07]}>
-                <View style={[{height:50, marginTop: statusBarHeight}, styles.bgWhite, styles.row, styles.alignItemsCenter, styles.justifyContentBetween, styles.px25, styles.borderBtmGrayDb, styles.hidden]}>
-                    <TouchableWithoutFeedback>
-                        <View style={[styles.pr20]}>
-                            <Image source={require('../../assets/images/icon_back.png')} style={[{width: 9*1.6, height: 17*1.6}]} />
-                        </View>
-                    </TouchableWithoutFeedback>
-                    <Text style={[styles.fontBold, styles.font18]}>통합 검색</Text>
-                    <TouchableWithoutFeedback>
-                        <View style={[styles.hidden]}>
-                            <Image source={require('../../assets/images/search.png')} style={[{width: 17*1.6, height: 17*1.6}]} />
-                        </View>
-                    </TouchableWithoutFeedback>
-                </View>
-                <View style={[styles.row, styles.alignItemsCenter, styles.justifyContentBetween, styles.px15, styles.py10, styles.bgTransparent, {zIndex: 9}]}>
-                    <TextInput 
-                    style={[styles.bgWhite, styles.black, styles.borderRadius5, styles.font14, styles.pl20, styles.width90, styles.justifyContentCenter, {zIndex: 9, height: 43}]} 
-                    underlineColorAndroid={'transparent'} 
-                    autoCapitalize={'none'} 
-                    autoCorrect={false} 
-                    value={props.q} 
-                    onChangeText={props.handleQChange} 
-                    returnKeyType={'send'} 
-                    onFocus={props.onFocus}
-                    onBlur={props.onBlur}
-                    autoFocus={true}
-                    />
-                    <TouchableWithoutFeedback onPress={props.onBlur}>
-                        <View style={[{zIndex: 99}]}>
-                            <Image source={require('../../assets/images/icon_close.png')} style={[styles.icon15]} />
-                        </View>
-                    </TouchableWithoutFeedback>
-                </View>
-            </View>
+              <View style={[{zIndex: 99}]}>
+                <Image
+                  source={require('../../assets/images/icon_close.png')}
+                  style={[styles.icon15]}
+                />
+              </View>
             </TouchableWithoutFeedback>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    )}
+    <View style={[styles.mt15, styles.px25]}>
+      <Text style={[styles.fontBold, styles.font16]}>작품</Text>
+      <View style={[{height: 230}]}>
+        {props.artworks && props.artworks.length > 0 ? (
+          <ScrollView
+            horizontal={true}
+            alwaysBounceVertical={false}
+            showsHorizontalScrollIndicator={false}
+            style={[styles.mt15]}>
+            {props.artworks.map((artwork, index) => (
+              <ArtworkCard
+                key={index}
+                artwork={artwork}
+                navigation={props.navigation}
+              />
+            ))}
+          </ScrollView>
+        ) : (
+          <View
+            style={[
+              styles.widthFull,
+              styles.heightFull,
+              styles.alignItemsCenter,
+              styles.justifyContentCenter,
+            ]}>
+            <Text
+              style={[
+                styles.fontMedium,
+                styles.font15,
+                styles.mt40,
+                styles.grayA7,
+                styles.textCenter,
+              ]}>
+              결과를 찾을 수 없습니다
+            </Text>
+          </View>
         )}
-        <View style={[styles.mt15, styles.px25]}>
-            <Text style={[styles.fontBold, styles.font16]}>작품</Text>
-            <View style={[{height: 230}]}>
-                {props.artworks && props.artworks.length > 0 ? (
-                    <ScrollView
-                    horizontal={true}
-                    alwaysBounceVertical={false}
-                    showsHorizontalScrollIndicator={false}
-                    style={[styles.mt15]}
-                    >
-                        {props.artworks.map((artwork, index) => (
-                            <ArtworkCard key={index} artwork={artwork} navigation={props.navigation} />
-                        ))}
-                    </ScrollView>
-                ) : (
-                    <View style={[styles.widthFull, styles.heightFull, styles.alignItemsCenter, styles.justifyContentCenter]}>
-                        <Text style={[styles.fontMedium, styles.font15, styles.mt40, styles.grayA7, styles.textCenter]}>결과를 찾을 수 없습니다</Text>
-                    </View>
-                )}
-            </View>
-        </View>
-        <View style={[styles.mt15, styles.px25]}>
-            <Text style={[styles.fontBold, styles.font16]}>전시</Text>
-            <View style={[{height: 300}]}>
-                {props.exhibitions && props.exhibitions.length > 0 ? (
-                    <ScrollView
-                    horizontal={true}
-                    alwaysBounceVertical={false}
-                    showsHorizontalScrollIndicator={false}
-                    style={[styles.mt15]}
-                    >
-                        {props.exhibitions.map((exhibition, index) => (
-                            <ExhibitionCard2 from={'Search'} key={index} exhibition={exhibition} navigation={props.navigation} />
-                        ))}
-                    </ScrollView>
-                ) : (
-                    <View style={[styles.widthFull, styles.heightFull, styles.alignItemsCenter, styles.justifyContentCenter]}>
-                        <Text style={[styles.fontMedium, styles.font15, styles.mt40, styles.grayA7, styles.textCenter]}>결과를 찾을 수 없습니다</Text>
-                    </View>
-                )}
-            </View>
-        </View>
+      </View>
     </View>
-)
+    <View style={[styles.mt15, styles.px25]}>
+      <Text style={[styles.fontBold, styles.font16]}>전시</Text>
+      <View style={[{height: 300}]}>
+        {props.exhibitions && props.exhibitions.length > 0 ? (
+          <ScrollView
+            horizontal={true}
+            alwaysBounceVertical={false}
+            showsHorizontalScrollIndicator={false}
+            style={[styles.mt15]}>
+            {props.exhibitions.map((exhibition, index) => (
+              <ExhibitionCard2
+                from={'Search'}
+                key={index}
+                exhibition={exhibition}
+                navigation={props.navigation}
+              />
+            ))}
+          </ScrollView>
+        ) : (
+          <View
+            style={[
+              styles.widthFull,
+              styles.heightFull,
+              styles.alignItemsCenter,
+              styles.justifyContentCenter,
+            ]}>
+            <Text
+              style={[
+                styles.fontMedium,
+                styles.font15,
+                styles.mt40,
+                styles.grayA7,
+                styles.textCenter,
+              ]}>
+              결과를 찾을 수 없습니다
+            </Text>
+          </View>
+        )}
+      </View>
+    </View>
+  </View>
+);
 
 SearchScreen.propTypes = {
-    q: PropTypes.string.isRequired,
-    handleQChange: PropTypes.func.isRequired,
-    onFocus: PropTypes.func.isRequired,
-    onBlur: PropTypes.func.isRequired,
-    focused: PropTypes.bool.isRequired,
-    artworks: PropTypes.array,
-    exhibitions: PropTypes.array,
-    makeBlank: PropTypes.func.isRequired
-}
+  q: PropTypes.string.isRequired,
+  handleQChange: PropTypes.func.isRequired,
+  onFocus: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  focused: PropTypes.bool.isRequired,
+  artworks: PropTypes.array,
+  exhibitions: PropTypes.array,
+  makeBlank: PropTypes.func.isRequired,
+};
 
 export default SearchScreen;
