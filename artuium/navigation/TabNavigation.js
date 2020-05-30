e7a01409267001251ec843d4101f72db03ffd9d9
KimSeungHu <mind9063@kaist.ac.kr>
2020-05-30 15:09:49 +0900

    shrink bottom tab area for iosX device

--- artuium/navigation/TabNavigation.js
+++ artuium/navigation/TabNavigation.js
@@ -1,30 +1,46 @@
 import React from 'react';
-import { View, Image } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import {View, Image} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import styles from '../styles';
 import HomeContainer from '../routes/HomeRoute';
 import ExhibitionContainer from '../routes/ExhibitionRoute';
 import ArtworkContainer from '../routes/ArtworkRoute';
 import ProfileContainer from '../routes/ProfileRoute';
+import {deviceInfo} from '../util';
 
-const TabNavigation = createBottomTabNavigator({
+const TabNavigation = createBottomTabNavigator(
+  {
     홈: {
-        screen: HomeContainer,
-        navigationOptions: {
-            header: null,
-            tabBarIcon: ({focused}) => (
-                <Image source={focused ? require('../assets/images/icon_home.png') : require('../assets/images/icon_home.png')} style={styles.iconTab} />
-            )
-        }
+      screen: HomeContainer,
+      navigationOptions: {
+        header: null,
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={
+              focused
+                ? require('../assets/images/icon_home.png')
+                : require('../assets/images/icon_home.png')
+            }
+            style={styles.iconTab}
+          />
+        ),
+      },
     },
     전시: {
-        screen: ExhibitionContainer,
-        navigationOptions: {
-            tabBarIcon: ({focused}) => (
-                <Image source={focused ? require('../assets/images/icon_exhibition.png') : require('../assets/images/icon_exhibition.png')} style={styles.iconTab} />
-            )
-        }
+      screen: ExhibitionContainer,
+      navigationOptions: {
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={
+              focused
+                ? require('../assets/images/icon_exhibition.png')
+                : require('../assets/images/icon_exhibition.png')
+            }
+            style={styles.iconTab}
+          />
+        ),
+      },
     },
     // 아트워크: {
     //     screen: ArtworkContainer,
@@ -35,34 +51,42 @@ const TabNavigation = createBottomTabNavigator({
     //     }
     // },
     MY: {
-        screen: ProfileContainer,
-        navigationOptions: {
-            tabBarIcon: ({focused}) => (
-                <Image source={focused ? require('../assets/images/icon_profile.png') : require('../assets/images/icon_profile.png')} style={styles.iconTab} />
-            )
-        }
-    }
-},
-{
+      screen: ProfileContainer,
+      navigationOptions: {
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={
+              focused
+                ? require('../assets/images/icon_profile.png')
+                : require('../assets/images/icon_profile.png')
+            }
+            style={styles.iconTab}
+          />
+        ),
+      },
+    },
+  },
+  {
     tabBarPosition: 'bottom',
     tabBarOptions: {
-        activeTintColor: '#000000',
-        inactiveTintColor: 'rgb(209,209,209)',
-        labelPosition: 'below-icon',
-        style: {
-            backgroundColor: '#ffffff',
-            height: 80,
-            borderTopColor: "rgb(209,209,209)",
-            paddingTop: 15,
-        },
-        labelStyle: {
-            fontSize: 10,
-            fontFamily: 'NotoSansKR-Medium',
-            includeFontPadding: false,
-            marginBottom: 20,
-        }
+      activeTintColor: '#000000',
+      inactiveTintColor: 'rgb(209,209,209)',
+      labelPosition: 'below-icon',
+      style: {
+        backgroundColor: '#ffffff',
+        height: deviceInfo.isIOSX ? 60 : 80,
+        borderTopColor: 'rgb(209,209,209)',
+        paddingTop: 15,
+      },
+      labelStyle: {
+        fontSize: 10,
+        fontFamily: 'NotoSansKR-Medium',
+        includeFontPadding: false,
+        marginBottom: deviceInfo.isIOSX ? 0 : 20,
+      },
     },
-});
+  },
+);
 
 const TabContainer = createAppContainer(TabNavigation);
 
